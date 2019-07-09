a4231a5fc042c871c65c224184f9b07d18cc860c
Manavsharma96 <ms.manavsharma123@gmail.com>
2019-07-09 11:24:38 +0530

    reverted

--- sec.py
+++ sec.py
@@ -2,8 +2,3 @@ print("This is git hub Welcome")
 print("sdfkjksf")
 print("NOW THE FILE IN GIT HUB SERVER")
 print("edited on 9/7")
-
-print("This is git hub Welcome")
-print("sdfkjksf")
-print("NOW THE FILE IN GIT HUB SERVER")
-print("edited on 9/7")
