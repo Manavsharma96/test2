767d1fd658ef394cb36483bee39202ca97ccfbe0
Manavsharma96 <ms.manavsharma123@gmail.com>
2019-07-09 11:44:47 +0530

    master merge

--- sec.py
+++ sec.py
@@ -3,6 +3,8 @@ print("sdfkjksf")
 print("NOW THE FILE IN GIT HUB SERVER")
 print("edited on 9/7")
 
+print("editing in master branch to merge")
+
 print("This is git hub Welcome")
 print("sdfkjksf")
 print("NOW THE FILE IN GIT HUB SERVER")
