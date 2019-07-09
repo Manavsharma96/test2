8f89cbfeb576de3184cab681ef9b63f9dca61c45
Manavsharma96 <ms.manavsharma123@gmail.com>
2019-07-09 11:55:30 +0530

    hdskj

--- sec.py
+++ sec.py
@@ -1,7 +1,6 @@
 print("This is git hub Welcome")
 print("sdfkjksf")
 print("NOW THE FILE IN GIT HUB SERVER")
-<<<<<<< HEAD
 print("edited on 9/7")
 
 print("editing in master branch to merge")
@@ -15,6 +14,4 @@ print("edited on 9/7")
 print("commit changes")
 
 print("New branch introduced ")
-=======
-print("edited on 9/7")
->>>>>>> a4231a5fc042c871c65c224184f9b07d18cc860c
+
